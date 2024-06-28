9df608ce2f9c96a8683b9ccda6d66d9a5b4e722c
Dh-Kh <vsevolod.zaychenko@gmail.com>
2024-06-28 10:54:24 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+
+class RoadWim(object):
+    
+    def __init__(self):
+        pass
+    
+    
+    
+if __name__ == "__main__":
+    pass
---
+++ preprocessor.py
@@ -0,0 +1,78 @@
+from pypdf import PdfReader
+from typing import List
+import tabula
+import os
+import re
+import pandas as pd
+
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64"
+
+class PdfExtract(object):
+    
+    """
+    WIM_звіт_2021_в3.pdf
+    Can be installed at http://bit.ly/wimrprt21 (14,5 Мб.).
+    """
+    
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+        
+    """
+    retrieves all pages that correspond to regex
+    """
+        
+    def _retrieve_pages(self, pattern_data: str) -> List[int]:
+        
+        #r'Середня швидкість руху на майданчику\s*(.*?)\s*у\s*2021\s*році'
+        
+        reader = PdfReader(self.file_name)
+        
+        pattern = re.compile(pattern_data)
+        
+        page_list = []
+        for index, page in enumerate(reader.pages):
+            text = page.extract_text()
+            if pattern.search(text):
+                page_list.append(index+1)
+        return page_list
+    
+    """
+    Generate CSV file in root folder
+    """
+    
+    def retrieve_tables(self, pattern_data: str) -> None:
+        
+        pages_data = self._retrieve_pages(pattern_data)
+        
+        tabula_options = {
+            "pages": pages_data,
+            "multiple_tables": True,
+            "guess": False,          
+            "lattice": True,        
+            "area": [280, 10, 1700, 800]  
+        }   
+          
+        dfs = tabula.read_pdf(self.file_name, **tabula_options)
+        
+        data_list = []
+        
+        for df in dfs:
+            headers = df.iloc[0].fillna("").tolist()
+            df = df.iloc[1:].copy()                 
+            df.columns = headers
+            df = df.T
+            df.insert(0, 'Тип транспортного засобу', df.index)  
+            df = df.reset_index(drop=True)   
+            data_list.append(df)
+
+        
+        df = pd.concat(data_list, ignore_index=True)
+        
+        df.to_csv("data.csv", index=False)
+    
+  
+      
+pdf = PdfExtract("WIM_звіт_2021_в3.pdf")
+
+pdf.retrieve_tables(r'Середня швидкість руху на майданчику\s*(.*?)\s*у\s*2021\s*році')
+    
