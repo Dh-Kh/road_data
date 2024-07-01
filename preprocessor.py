b6c6a210f48884059b1f09aa2a17b65864d15635
Dh-Kh <vsevolod.zaychenko@gmail.com>
2024-07-01 10:20:25 +0300

    third commit

--- preprocessor.py
+++ preprocessor.py
@@ -1,11 +1,8 @@
 from pypdf import PdfReader
 from typing import List
-import tabula
-import os
+import camelot
 import re
-#import pandas as pd
 
-os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64"
 
 class PdfExtract(object):
     
@@ -23,44 +20,66 @@ class PdfExtract(object):
         
     def _retrieve_pages(self, pattern_data: str) -> List[int]:
         
-        #r'Середня швидкість руху на майданчику\s*(.*?)\s*у\s*2021\s*році'
-        
         reader = PdfReader(self.file_name)
         
         pattern = re.compile(pattern_data)
-        
+                
         page_list = []
+
         for index, page in enumerate(reader.pages):
             text = page.extract_text()
             if pattern.search(text):
-                page_list.append(index+1)
+                if "Середня швидкість руху  за проміжками часу, км/год" in text:
+                    page_list.append(index+1)
         return page_list
     
     """
     Generate CSV file in root folder
     """
     
-    def retrieve_tables(self, pattern_data: str) -> None:
+    def retrieve_tables(self) -> List[List[List[int]]]:
         
-        pages_data = self._retrieve_pages(pattern_data)
+        pages_data = self._retrieve_pages(
+            r'Середня швидкість руху на майданчику\s*(.*?)\s*у\s*2021\s*році'
+        )
         
-        tabula_options = {
-            "pages": pages_data,
-            "guess": False,
-            "lattice": True,
-            "stream": True,
-        }
-        dfs = tabula.read_pdf(self.file_name, **tabula_options)
+        """
+        global_stack = []
+        
+        for page in pages_data:
+            
+            tables = camelot.read_pdf("WIM_звіт_2021_в3.pdf", pages=str(page))
 
-        #inccorect data retrieves        
+            if tables:
+                
+                table = tables[0]
+
+                df = table.df.drop(columns=table.df.columns[:2], axis=1)
+
+                df.reset_index(drop=True, inplace=True)
+
+                stack = []
+
+                stack.append(df.iloc[2])
+
+                stack = [value for value in stack[0].values]
+
+                array = stack[0].split()
+
+                array = [0 if x == "-" else x for x in array]    
+            
+                n = 24
+
+                array = [array[i:i+n] for i in range(0, len(array), n)]
+
+                global_stack.append(array)
+        """
+        #return global_stack
+    
   
 if __name__ == "__main__":
-    """      
     pdf = PdfExtract("WIM_звіт_2021_в3.pdf")
-
-    pdf.retrieve_tables(r'Середня швидкість руху на майданчику\s*(.*?)\s*у\s*2021\s*році')
+    print("Waiting...")
+    pdf.retrieve_tables()
+    print(pdf.retrieve_tables())
     
-    """
-    pdf = PdfExtract("WIM_звіт_2021_в3.pdf")
-    print(pdf.retrieve_tables(r'Середня швидкість руху на майданчику\s*(.*?)\s*у\s*2021\s*році'))
-    pass
